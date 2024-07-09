3d16579602afb468ad2caaccd879c4111ea6e175
uAtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2024-07-09 22:14:47 +0200

    Remove Nuxt UI from dependencies.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ export default defineNuxtConfig({
 	devtools: { enabled: true },
 	ssr: false,
 	modules: [
-		'@nuxt/ui',
 		'@nuxtjs/eslint-module',
+		'@nuxt/icon',
+		'@nuxtjs/tailwindcss',
 	],
 });
