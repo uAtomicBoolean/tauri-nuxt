1fae49871f629e57b3119ab6f82afd5bd7b3584e
AtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2024-10-22 10:42:40 +0200

    chore: Clean content in `tailwind.config.js`

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,10 @@ import colors from 'tailwindcss/colors';
 
 export default {
 	content: [
-		'src/components/**/*.{vue,js,ts}',
-		'src/layouts/**/*.vue',
-		'src/pages/**/*.vue',
-		'src/composables/**/*.{js,ts}',
-		'src/plugins/**/*.{js,ts}',
-		'src/utils/**/*.{js,ts}',
-		'src/App.{js,ts,vue}',
-		'src/app.{js,ts,vue}',
-		'src/Error.{js,ts,vue}',
-		'src/error.{js,ts,vue}',
-		'src/app.config.{js,ts}',
+		'./pages/**/*.{ts,tsx,vue}',
+		'./components/**/*.{ts,tsx,vue}',
+		'./app/**/*.{ts,tsx,vue}',
+		'./src/**/*.{ts,tsx,vue}',
 	],
 	theme: {
 		extend: {
